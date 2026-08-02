c15cef5ff95033dd84d50cbd6bbd5ddb0a46eb6c
Aditya Prakash <prakashaditya2709@gmail.com>
2026-08-02 15:53:08 +0530

    Time: 342 ms (27.74%), Space: 137.7 MB (15.49%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,13 @@
+from functools import cache
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        @cache
+        def dfs(l, r):
+            if l == r:
+                return piles[l]
+                
+            return max(
+                piles[l] - dfs(l+1, r),
+                piles[r] - dfs(l, r-1)
+            )
+        return dfs(0, len(piles) - 1) > 0
